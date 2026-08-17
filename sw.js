bc9c1f5d5fd6455f9fd229cbe2c87827826dd997
SaintZeroVoid <crisninopilaza@gmail.com>
2026-08-17 11:04:23 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,11 @@
-const CACHE = 'heavenly-dao-v4-loaderfix
-const ASSETS = [
-  './',
-  './index.html',
-  './css/styles.css',
-  './css/design-system.css',
-  './js/data.js',
-  './js/app.js',
-  './js/factions-extra.js',
-  './js/sim.worker.js',
-  './js/modules/ui.js',
-  './js/modules/combat.js',
-  './js/modules/graph.js',
-  './js/modules/lineage.js',
-  './js/modules/story.js',
-  './manifest.json'
-];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
-});
+// Temporary: clear old caches and unregister so broken app.js cannot stick
+self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
   );
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
